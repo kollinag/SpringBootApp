a46b32cc0ecedd3b26549c3be6562b819993002d
Kolli <52082153@acsind.com>
2020-11-22 22:33:55 +0530

    feature 300 chnages

--- src/main/java/com/kolli/Application.java
+++ src/main/java/com/kolli/Application.java
@@ -9,6 +9,7 @@ import com.kolli.util.PasswordEncryptor;
 @SpringBootApplication
 public class Application {
 
+System.out.println("hello nagarjuna how are you man this is testing of conflict in git");
 	public static void main(String[] args) {
 		
 		//Hello nagarjuna how are you i am fine
